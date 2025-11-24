a6ff461b2a57dd1ea2123323e0ebb51d661139e7
Danila Starikov <1132226531@pfur.ru>
2025-11-24 16:19:13 +0300

    feat(main): luneberg lens modeling
    - `IndexProf_2000_points.txt` -- has n_eff index for lens
    - `luneberg_lens_numbers` -- has n_eff index and computed beta and dh
    numbers
    - benchmarked two variants to compute determinant

--- src/4layer.py
+++ src/4layer.py
@@ -7,6 +7,7 @@ from sympy.vector import CoordSys3D, Del
 
 import numpy as np
 from scipy.optimize import bisect
+import matplotlib.pyplot as plt
 
 from preview_wrappers import preview_collection, save_latex_as_image
 from solveHomoSLAE import solve_all
@@ -207,7 +208,7 @@ def layered_sols(sols, layer_symbol, order) -> list:
                 sols = [
                     sol.replace(
                         sp.exp,
-                        lambda arg: sp.exp(arg.subs({x: x - sp.Function("h1")(z)})),
+                        lambda arg: sp.exp(arg.subs({x: x - sp.Function("h_l")(z)})),
                     )
                     for sol in sols
                 ]
@@ -225,7 +226,31 @@ def layered_sols(sols, layer_symbol, order) -> list:
                 eval=False,
             )
 
-        case "f" | "l":
+        case "l":
+            if order == 0:
+                sols = [
+                    sol.replace(
+                        sp.exp,
+                        lambda arg: sp.exp(arg.subs({x: x - sp.Function("h_f")(z)})),
+                    )
+                    for sol in sols
+                ]
+
+            sols = list_subs(
+                sols,
+                {
+                    sp.Symbol("C1"): sp.Symbol(f"A_{order}^{layer_symbol}"),
+                    sp.Symbol("C2"): sp.Symbol(f"B_{order}^{layer_symbol}"),
+                    sp.Symbol("C3"): sp.Symbol(f"C_{order}^{layer_symbol}"),
+                    sp.Symbol("C4"): sp.Symbol(f"D_{order}^{layer_symbol}"),
+                }
+                | prev_U_subs
+                | U_p_subs,
+                eval=False,
+            )
+
+
+        case "f":
             sols = list_subs(
                 sols,
                 {
@@ -436,48 +461,48 @@ if __name__ == "__main__":
 
     # for 4 layer slab waveguide:
     #     cover
-    #     ---- x=h_1
+    #     ---- x=h_l=h_f+dh
     #     lens
-    #     ---- x=h_2
+    #     ---- x=h_f
     #     film
     #     ---- x=0
     #     substrate
 
     
-    h1 = sp.Function("h1")
-    h2 = sp.Function("h2")    
+    h_l = sp.Function("h_l")
+    h_f = sp.Function("h_f")
     border_func = lambda f: R.x - f(R.z)
 
     E_boundry_cl_0 = gen_boundry_conds(
-        R, delop, E_vec_comps[0], border_func(h1), h1(z), ["c", "l"], order=0
+        R, delop, E_vec_comps[0], border_func(h_l), h_l(z), ["c", "l"], order=0
     )
 
     H_boundry_cl_0 = gen_boundry_conds(
-        R, delop, H_vec_comps[0], border_func(h1), h1(z), ["c", "l"], order=0
+        R, delop, H_vec_comps[0], border_func(h_l), h_l(z), ["c", "l"], order=0
     )
 
     E_boundry_lf_0 = gen_boundry_conds(
-        R, delop, E_vec_comps[0], border_func(h2), h2(z), ["l", "f"], order=0
+        R, delop, E_vec_comps[0], border_func(h_f), h_f(z), ["l", "f"], order=0
     )
 
     H_boundry_lf_0 = gen_boundry_conds(
-        R, delop, H_vec_comps[0], border_func(h2), h2(z), ["l", "f"], order=0
+        R, delop, H_vec_comps[0], border_func(h_f), h_f(z), ["l", "f"], order=0
     )
 
     E_boundry_fs_0 = gen_boundry_conds(
-        R, delop, E_vec_comps[0], border_func(h2), 0, ["f", "s"], order=0
+        R, delop, E_vec_comps[0], border_func(h_f), 0, ["f", "s"], order=0
     )
 
     H_boundry_fs_0 = gen_boundry_conds(
-        R, delop, H_vec_comps[0], border_func(h2), 0, ["f", "s"], order=0
+        R, delop, H_vec_comps[0], border_func(h_f), 0, ["f", "s"], order=0
     )
 
     boundry_eqs_0 = list_subs(
         E_boundry_cl_0 + H_boundry_cl_0 + E_boundry_lf_0 + H_boundry_lf_0 + E_boundry_fs_0 + H_boundry_fs_0,
         {
             eq.lhs: eq.rhs
-            for eq in list_subs(sols_0_layers["c"] + sols_0_layers["l"], {x: h1(z)}, eval=False)
-            +list_subs(sols_0_layers["l"] + sols_0_layers["f"], {x: h2(z)}, eval=False)
+            for eq in list_subs(sols_0_layers["c"] + sols_0_layers["l"], {x: h_l(z)}, eval=False)
+            +list_subs(sols_0_layers["l"] + sols_0_layers["f"], {x: h_f(z)}, eval=False)
             +list_subs(sols_0_layers["f"] + sols_0_layers["s"], {x: 0}, eval=False)
 
         },
@@ -569,48 +594,62 @@ if __name__ == "__main__":
         symbolic_subs[add_layer_index(eta, layer)(z)] = -epsilon_layer * mu_layer + sp.diff(phi(z), z) ** 2
         symbolic_subs[add_layer_index(gamma, layer)(z)] = omega / c * sp.sqrt(-epsilon_layer * mu_layer + sp.diff(phi(z), z) ** 2)
 
-    num_lambda = sp.sympify(0.55)
+    num_lambda = sp.Float(0.55)
     beta = sp.Symbol('beta')
-    numeric_parameters = {sp.Symbol('epsilon_c') : sp.sympify(1.0)**2,
-                           sp.Symbol('epsilon_l') : sp.sympify(3.61),
-                           sp.Symbol('epsilon_f') : sp.sympify(1.565)**2,
-                           sp.Symbol('epsilon_s') : sp.sympify(1.47)**2,
-                           h1(z)                   : 1.47,
-                           h2(z)                   : 2*num_lambda,
-                           sp.Symbol('mu_c')      : sp.sympify(1.0),
-                           sp.Symbol('mu_l')      : sp.sympify(1.0),
-                           sp.Symbol('mu_f')      : sp.sympify(1.0),
-                           sp.Symbol('mu_s')      : sp.sympify(1.0),
-                           omega/c                : 2*sp.pi/num_lambda,
-                           sp.diff(phi(z),z)**2   : beta**2,
-                           }
+    dh = sp.Symbol(r'\delta h')
+    numeric_parameters = {
+        sp.Symbol('epsilon_c') : sp.Float(1.0)**2,
+        sp.Symbol('epsilon_l') : sp.Float(3.61),
+        sp.Symbol('epsilon_f') : sp.Float(1.565)**2,
+        sp.Symbol('epsilon_s') : sp.Float(1.47)**2,
+        h_l(z)                 : 2*num_lambda+dh,    # форма линзы h_l=h_f+dh
+        h_f(z)                 : 2*num_lambda,
+        sp.Symbol('mu_c')      : sp.Float(1.0),
+        sp.Symbol('mu_l')      : sp.Float(1.0),
+        sp.Symbol('mu_f')      : sp.Float(1.0),
+        sp.Symbol('mu_s')      : sp.Float(1.0),
+        omega/c                : 2*sp.pi/num_lambda,
+        sp.diff(phi(z),z)**2   : beta**2,
+    }
        
 
-    # M_0_TE = M_0_TE.subs(symbolic_subs)
-    print('getting det TE mode')
-    det_TE = M_0_TE.subs(symbolic_subs).subs(numeric_parameters).doit()#.det().simplify()
-    det_TE_numerical = sp.lambdify(beta, sp.im(det_TE), modules=[{'sqrt':np.emath.sqrt},'numpy'])
+    M_0_TE_num = sp.lambdify([beta, dh], M_0_TE.subs(symbolic_subs).subs(numeric_parameters).doit(), 
+                                   modules=[{'sqrt':np.emath.sqrt},'numpy'])
+    def get_determinant_TE(beta, dh):
+        M = M_0_TE_num(beta, dh)
+        det = np.linalg.det(M)
+        return det.imag+det.real
 
-    quit()
+    lens_index_path = "IndexProf_2000_points.txt"
+    if True: # do we need to recompute lens profile?
+        print(f"computing lens profile from index profile in {lens_index_path}")
+        with open(lens_index_path, 'r') as f:
+            lines = f.readlines()[2:]
+            data = []
+            for line in lines:
+                data.append(list(map(float,line.split())))
 
-    # M_0_TM = M_0_TM.subs(symbolic_subs)
-    print('getting det TM mode')    
-    det_TM = M_0_TM.subs(symbolic_subs).subs(numeric_parameters).doit().det().simplify()
-    det_TM_numerical = sp.lambdify(beta, sp.im(det_TM), modules=[{'sqrt':np.emath.sqrt},'numpy'])
+            data = data[::-1] # начинаем с r=1 -- край линзы
 
-    beta_results = [
-        bisect(det_TE_numerical, 1.54, 1.56, xtol=1e-16),
-        bisect(det_TE_numerical, 1.50, 1.52, xtol=1e-16),
-        bisect(det_TM_numerical, 1.54, 1.56, xtol=1e-16),
-        bisect(det_TM_numerical, 1.50, 1.52, xtol=1e-16)
-    ]
 
-    # check beta with results from https://link.springer.com/chapter/10.1007/978-3-030-26831-2_8
-    beta_previous_results = [1.5514927380692901290,
-                             1.5117506145374407068,
-                             1.5501811158901042864,
-                             1.5072749512764207624]
-    print(f"Comparison with previous results:")
-    print(f"old\tnew\terr")
-    for beta_old, beta_new in zip(beta_previous_results, beta_results):
-        print(beta_old, beta_new, abs(beta_old-beta_new))
+
+        # beta_0 это к.ф.з. для трехслойного волновода, на границе линзы, когда dh=0 
+        beta_0 = bisect(lambda b: get_determinant_TE(b, 0), 1.54, 1.6, xtol=1e-16)
+
+        dh = [0.]
+        beta = [beta_0]
+        for i in range(1, len(data)):
+            beta.append(beta_0*data[i][1]) # beta=beta_0*n_eff
+            dh.append(bisect(lambda dh: get_determinant_TE(beta[i], dh), dh[i-1], dh[i-1]+.2, xtol=1e-16))
+    
+    
+        plt.figure(figsize=(20,6))
+        plt.plot(np.linspace(-1,1, len(dh)*2), list(map(lambda dh: dh+2*num_lambda, dh+dh[::-1])))
+        plt.savefig("../images/lens_profile.png")
+
+        # saving luneberg lens profile 
+        with open('luneberg_lens_profile.txt', 'w') as f:
+            f.write('r, n_eff(r,f), dh, beta\n')
+            for i in range(len(data)):
+                f.write(f'{data[i][0]},{data[i][1]},{dh[i]},{beta[i]}\n')
+
