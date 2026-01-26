ecec58c942c6b80b7aaa60e58536193d87ea1d0e
Danila Starikov <1132226531@pfur.ru>
2026-01-26 20:43:42 +0300

    feat(main): added construction of layered solutions

--- pyawm/main.py
+++ pyawm/main.py
@@ -24,12 +24,27 @@ x       = sp.Symbol("x")
 y       = sp.Symbol("y")
 z       = sp.Symbol("z")
 phi     = sp.Function("varphi")(y,z)
-eta     = sp.Function("eta")
-gamma   = sp.Function("gamma")
+eta     = sp.Function("eta")(y,z)
+gamma   = sp.Function("gamma")(y,z)
 c       = sp.Symbol("c")
 mu      = sp.Symbol("mu")
 epsilon = sp.Symbol("varepsilon")
 omega   = sp.Symbol("omega")
+
+from enum import Enum
+
+class Layer(Enum):
+    # strings will be used as indexes in sympy object names
+    COVER      = "c"
+    FILM       = "f"
+    LENS       = "l"
+    SUBSTRATE  = "s"
+
+    # Define aliases as class attributes
+    C = COVER
+    F = FILM
+    L = LENS
+    S = SUBSTRATE
 
 class Waveguide:
     """
@@ -39,11 +54,12 @@ class Waveguide:
     3. Geometry: how many layers, their name/indexes, function that describes their profile
     4. Phase `phi` with correct arguments 
     """
-    def __init__(self, non_zero_vars, layers, geometry, phi):
-        self.non_zero_vars = non_zero_vars
+    def __init__(self, field_vars, layers, geometry, phi_vars):
+        self.field_vars = field_vars
         self.layers        = layers
         self.geometry      = geometry
-        self.phi           = phi
+        self.phi_vars      = phi_vars
+        self.phi           = phi.func(*phi_vars)
 
         
 class Domain:
@@ -105,9 +121,6 @@ class Domain:
         return eqs
 
     def construct_equations_wrt_series(self, order):
-        # E_series, H_series = self.get_series_sum(order)
-
-        # U_comps = [self.__getattribute__(U.name+comp.name) for U, comp in product([self.E, self.H], [x,y,z])]
         self.construct_series_sum(order)
         eqs = list_subs(self._maxwell_equations,
                           {
@@ -127,15 +140,18 @@ class Domain:
 
     def update_general_solution_form_wrt_waveguide(self, eqs, order):
         """
-        substitute (x,y,z)->WG.non_zero_vars, phi->WG.phi, 
+        substitute (x,y,z)->WG.field_vars, phi->WG.phi, 
         """
         sym_subs = dict()
-        sym_subs[phi] = WG.phi
+        sym_subs[phi] = self.WG.phi
 
+        # changes (x,y,z)->WG.field_vars and changes corresponding class attributes
         for U, comp, s in product(self.U, [x,y,z], range(0,order+1)):
             func = getattr(self, U.name+comp.name+str(s))
-            sym_subs[func] = func.func(*WG.non_zero_vars)
+            sym_subs[func] = func.func(*self.WG.field_vars)
+            setattr(self, U.name+comp.name+str(s), sym_subs[func])
 
+        
         # derivatives are zeroed only for d/dy and d/dz for highest components
         for U, comp in product(self.U, [x,y,z]):
             func = getattr(self, U.name+comp.name+str(order))
@@ -144,6 +160,72 @@ class Domain:
 
         return list_subs(eqs, sym_subs)
         
+
+    def layered_sols(self, sols, layer: Layer, order) -> list:
+        # NOTE: in some layers we make a substitution of x->x-h_i(z). 
+        # The layers to do this subs are chosen manually, and for now i dont know how to automate this criteria.
+        # Maybe just provide a flag for each layer to do or not to do this substitution.
+
+        # 2. Zero coeffs at exp(-<...>) or exp(<..>) governed by asymptotic conditions on infty
+        match layer:
+            case Layer.COVER:
+                sols = {k: v.replace(sp.exp( x*gamma.func(*self.WG.phi.args)), 0) for k, v in sols.items()}
+            case Layer.SUBSTRATE:
+                sols = {k: v.replace(sp.exp(-x*gamma.func(*self.WG.phi.args)), 0) for k, v in sols.items()}
+
+            # For film and lens layers are closed, so asympotic conditions dont apply
+            case Layer.FILM | Layer.FILM:
+                pass
+                
+
+
+        # 1. Subs functions and constants in accordance to layer passed
+        sym_subs = dict()
+        # constants
+        setattr(self, mu.name+"_"+layer.value, sp.Symbol(f"{mu.name}_{layer.value}"))
+        setattr(self, epsilon.name+"_"+layer.value, sp.Symbol(f"{epsilon.name}_{layer.value}"))
+        setattr(self, eta.name+"_"+layer.value, sp.Function(f"{eta.name}_{layer.value}")(*self.WG.phi_vars))
+        setattr(self, gamma.name+"_"+layer.value, sp.Function(f"{gamma.name}_{layer.value}")(*self.WG.phi_vars))
+        sym_subs[mu]      = getattr(self, mu.name+"_"+layer.value)
+        sym_subs[epsilon] = getattr(self, epsilon.name+"_"+layer.value)
+        sym_subs[eta]     = getattr(self, eta.name+"_"+layer.value)
+        sym_subs[gamma]   = getattr(self, gamma.name+"_"+layer.value)
+        
+        for U, comp, s in product(self.U, [x,y,z], range(order+1)):
+            func = getattr(self, U.name+comp.name+str(s)) 
+            setattr(self, U.name+comp.name+str(s)+layer.value, sp.Function(f"{U}^{{{comp.name},{layer.value}}}_{s}")(*func.args))
+            sym_subs[func] = getattr(self, U.name+comp.name+str(s)+layer.value)
+
+        sols = dict_subs(sols, sym_subs, both=True)
+
+        # 3. Subs x->x-h_i in layers h != 0
+        match layer:
+            case Layer.COVER:
+                # TODO: this is ugly
+                if Layer.LENS in self.WG.layers:
+                    sols = dict_subs(sols, {x: x-sp.Function(f"h_{{{Layer.LENS.value}}}")(*self.WG.phi_vars)})
+                else:
+                    sols = dict_subs(sols, {x: x-sp.Function(f"h_{{{Layer.FILM.value}}}")(*self.WG.phi_vars)})
+            case Layer.LENS:
+                sols = dict_subs(sols, {x: x-sp.Function(f"h_{{{Layer.FILM.value}}}")(*self.WG.phi_vars)})
+            case Layer.FILM:
+                sols = dict_subs(sols, {x: x-sp.Function(f"h_{{{Layer.SUBSTRATE.value}}}")(*self.WG.phi_vars)})
+            case Layer.SUBSTRATE:
+                # do nothing
+                pass
+
+        # 4. Subs C1, C2,.. with lettered coeffs
+
+        C_numbers = sp.symbols("C1:5")
+        sym_coeffs = list()
+        for letter in "ABCD":
+            setattr(self, letter+layer.value+str(order), sp.Symbol(f"{letter}^{layer.value}_{order}"))
+            sym_coeffs.append(getattr(self, letter+layer.value+str(order)))
+
+        setattr(self, f"sym_coeffs{order}", sym_coeffs)
+        sols = dict_subs(sols, {k: v for k, v in zip(C_numbers, sym_coeffs, strict=True)})
+
+        return sols
     
     def solve_zero_order(self):
         order = 0
@@ -160,14 +242,14 @@ class Domain:
         self.alg_eqs0  = [eq for eq in d.eqs0 if not eq.find(sp.Derivative(sp.Wild('w'), x))]
 
         # TODO: is it too "hard-coded"? Maybe its fine
-        self.alg_sols0 = {self.Hx0.func(x,z): sp.solve(self.alg_eqs0[0], self.Hx0.func(x,z))[0],
-                          self.Ex0.func(x,z): sp.solve(self.alg_eqs0[1], self.Ex0.func(x,z))[0]}
+        self.alg_sols0 = {self.Hx0: sp.solve(self.alg_eqs0[0], self.Hx0)[0],
+                          self.Ex0: sp.solve(self.alg_eqs0[1], self.Ex0)[0]}
 
         # temporary substitute all functions' arguments (x,y,z) -> (x)
         sym_func_subs = dict()
         for U, comp in product(self.U, [y, z]):
             func = getattr(self, U.name+comp.name+str(order))
-            sym_func_subs[func.func(*self.WG.non_zero_vars)] = func.func(x)
+            sym_func_subs[func] = func.func(x)
 
         temp_diff_eqs = list_subs(self.diff_eqs0, self.alg_sols0)
         temp_diff_eqs = list(map(lambda eq: eq.doit(), temp_diff_eqs)) # auto reduces d/dy phi(z) 
@@ -185,58 +267,55 @@ class Domain:
         for sol in self.diff_sols0:
             self.sols0[sol.lhs] = sol.rhs
 
+        # TODO: generalize for lens problem 
         self.sols0 = dict_subs(
             self.sols0,
             {
-                omega / c * sp.sqrt(-epsilon * mu + sp.diff(self.WG.phi, z) ** 2): gamma(z),
-                -epsilon * mu + sp.diff(self.WG.phi, z) ** 2: eta(z),
+                omega / c * sp.sqrt(-epsilon * mu + sp.diff(self.WG.phi, z) ** 2): gamma,
+                -epsilon * mu + sp.diff(self.WG.phi, z) ** 2: eta
             },
             eval=False,
         )
-            
 
+        self.sols0[self.Ex0] = self.sols0[self.Ex0].subs(self.sols0)
+        self.sols0[self.Hx0] = self.sols0[self.Hx0].subs(self.sols0)
+        
+        # multiple by a denominator to simplify form
         self.sols0 = dict_subs(
             self.sols0,
             {
-                sp.Symbol("C1"): sp.Symbol("C1") * sp.sqrt(eta(z)),
-                sp.Symbol("C2"): sp.Symbol("C2") * sp.sqrt(eta(z)),
+                sp.Symbol("C1"): sp.Symbol("C1") * sp.sqrt(eta),
+                sp.Symbol("C2"): sp.Symbol("C2") * sp.sqrt(eta),
                 sp.Symbol("C3"): sp.Symbol("C3") * epsilon,
                 sp.Symbol("C4"): sp.Symbol("C4") * epsilon,
             },
             eval=False,
         )
-        preview_collection(self.sols0)
+        # preview_collection(self.sols0)
 
-from enum import Enum
-
-class Layer(Enum):
-    # strings will be used as indexes in sympy object names
-    COVER      = "c"
-    FILM       = "f"
-    LENS       = "l"
-    SUBSTRATE  = "s"
-
-    # Define aliases as class attributes
-    C = COVER
-    F = FILM
-    L = LENS
-    S = SUBSTRATE
+        # Construct solution in each layer
+        for layer in self.WG.layers:
+            setattr(self, "sols0"+"_"+layer.value, self.layered_sols(self.sols0, layer, order))
     
     
 if __name__ == "__main__":
     # 2d smoothly irregular waveguide, ref: https://indico.jinr.ru/event/4467/contributions/28934/attachments/20530/35707/starikov_mmcp2024.pdf
-    non_zero_vars = [x, z]
+    field_vars = [x, z]
+    # TODO: change name
+    phi_vars = [z] # and for h(z)
     layers = [
         Layer.SUBSTRATE,
         Layer.FILM,
         Layer.COVER,
     ]
+
+    # TODO: think about autogenerating h function and provide only layers.
+    # Less manual work and should be easier to maintain
     geometry = [
-        (Layer.SUBSTRATE, Layer.FILM, 0),
-        (Layer.COVER, Layer.FILM, sp.Function(f"h_{{{Layer.C.name}{Layer.F.name}}}"))
+        (Layer.FILM,      Layer.COVER, sp.Function(f"h_{{{Layer.FILM.value}}}")),
+        (Layer.SUBSTRATE, Layer.FILM,  0)
     ]
-    
-    WG = Waveguide(non_zero_vars, layers, geometry, phi.func(z))
-    d = Domain(WG)
+    horn = Waveguide(field_vars, layers, geometry, phi_vars)
+    d = Domain(horn)
     d.solve_zero_order()
     pass
